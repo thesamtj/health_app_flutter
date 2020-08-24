6d39d4edb854445c07fdbcbe282a105c49f39297
Samuel Tijani <omalitijani@gmail.com>
2020-08-24 18:19:13 +0100

    Set color & box properties

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,13 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        height: 200.0,
+        width: 170.0,
+        decoration: BoxDecoration(
+            color: Color(0xFF1D1E33),
+            borderRadius: BorderRadius.circular(10.0)),
       ),
     );
   }
