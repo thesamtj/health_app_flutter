b42642546e24c52ae3a5cbceaad4c0fc4080161a
Samuel Tijani <omalitijani@gmail.com>
2020-08-24 20:51:07 +0100

    Add colour property

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,7 +18,7 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: <Widget>[
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
               ),
               Expanded(
                 child: ReusableCard(),
@@ -46,16 +46,16 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
+  Color colour;
+
+  ReusableCard({this.colour});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color: Color(0xFF1D1E33), borderRadius: BorderRadius.circular(10.0)),
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
