bba07c6a22301c03e2e0be30b5ec1c542ffee946
Samuel Tijani <omalitijani@gmail.com>
2020-08-25 09:07:53 +0100

    Add child property

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -57,12 +57,14 @@ class _InputPageState extends State<InputPage> {
 
 class ReusableCard extends StatelessWidget {
   final Color colour;
+  final Widget cardChild;
 
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           color: colour, borderRadius: BorderRadius.circular(10.0)),
