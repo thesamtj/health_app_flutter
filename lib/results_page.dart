008a9dd49cf6bbfda5e6ae27f4f4191407101acf
Samuel Tijani <omalitijani@gmail.com>
2020-08-27 19:15:52 +0100

    Implement navigation

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ResultPage extends StatelessWidget {
+class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
