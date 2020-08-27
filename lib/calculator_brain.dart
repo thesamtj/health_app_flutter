81a97c8aab1bbbc1f391d1935074319fdea02b92
Samuel Tijani <omalitijani@gmail.com>
2020-08-27 21:25:52 +0100

    Calculate BMI

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -6,4 +6,9 @@ class CalculatorBrain {
   double _bmi;
 
   CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
 }
