d2b9657acfb940c230053fa418bed7ec8314100b
Samuel Tijani <omalitijani@gmail.com>
2020-08-27 21:05:44 +0100

    Set params

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -223,22 +223,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Function onPressed;
-
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 0.0,
-      constraints: BoxConstraints.tightFor(width: 50.0, height: 50.0),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
