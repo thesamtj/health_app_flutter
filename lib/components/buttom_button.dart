6bdf0f131c395975daac17f0c1e11427926d04d2
Samuel Tijani <omalitijani@gmail.com>
2020-08-27 20:51:09 +0100

    Refactor

---
+++ lib/components/buttom_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final Function onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ResultsPage()),
+        );
+      },
+      child: Container(
+        child: Center(
+          child: Text(
+            'CALCULATE',
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
